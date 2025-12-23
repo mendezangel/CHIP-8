9623c15f2dd3e4a862f80ba692184a67112ff058
Angel Mendez <id.angelmendez@gmail.com>
2025-12-22 20:39:24 -0800

    added test file for chip8.c, currently only implemented function to test chip8 initialization function

---
+++ tests/test_chip8.c
@@ -0,0 +1,16 @@
+#include "../src/chip8.h"
+#include <assert.h>
+
+void test_chip8_init() {
+    Chip8 chip8;
+    chip8_init(&chip8);
+
+    assert(chip8.pc == 0x200); // PC starts at 0x200
+    assert(chip8.sp == 0);     // Stack pointer is 0
+    assert(chip8.I == 0);      // Index register is 0
+
+    // Check font is loaded
+    assert(chip8.memory[FONTSET_START_ADDRESS] == 0xF0); // First byte of '0' character
+    
+    printf("test_chip8_init passed \n");
+}
